8d0d416229c8a9cb6f4220ad7a374f2852ef9e9f
RMohit927 <86957308+RMohit927@users.noreply.github.com>
2021-09-09 18:20:37 +0530

    Update Remove Element.cpp

--- Week1/Remove Element.cpp
+++ Week1/Remove Element.cpp
@@ -35,12 +35,12 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         int j=0;
-		for(int i=0;i<nums.size();i++){
-			if(nums[i]!=val){
-				nums[j++]=nums[i];
-			}
+	for(int i=0;i<nums.size();i++){
+		if(nums[i]!=val){
+			nums[j++]=nums[i];
 		}
-		return j;
+	}
+	return j;
     }
 };
 
