5025b44317b4a29eae5f2191f548257a662fb5c3
RMohit927 <86957308+RMohit927@users.noreply.github.com>
2021-09-22 16:34:36 +0530

    Update Unique Paths II.cpp

--- Week2/Unique Paths II.cpp
+++ Week2/Unique Paths II.cpp
@@ -13,7 +13,7 @@ Example 1:
     2. Down -> Down -> Right -> Right
 */
 
-/* Approach: Complexity - O(R*C) - Dynamic Programming*/
+/* Approach: Complexity - O(R*C) - Dynamic Programming - Memoization*/
 
 class Solution {
 public:
@@ -44,3 +44,36 @@ public:
 0,0,0,0
 */
 
+/* Approach: Complexity - O(N*M) - DP - Tabular Method */
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int n = grid.size() , m = grid[0].size();
+        
+		//if the source or destination contains an obstacle , then answer is 0.
+        if(grid[n-1][m-1] == 1 or grid[0][0] == 1) return 0;
+        
+        int dp[n+1][m+1];
+		//initialise dp table with 0.
+        memset(dp,0,sizeof(dp));
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+				//1 way from source to source is to stay (we have checked earlier for obstacle at source)
+                if(i == 1 and j == 1) dp[i][j] = 1;
+				
+				//if curr sqaure doesn't contain obstacle
+                else if(grid[i-1][j-1] != 1)
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+				
+				//if the curr square conatins obstacle
+                else dp[i][j] = 0;
+            }
+        }
+        
+        return dp[n][m];
+    }
+};
